897502c7eafa09f2b0aad1f8441d315f5b9032b8
Sebastiaan Jansen <contact@sebastiaanjansen.be>
2019-08-17 12:38:58 +0200

    upload example

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React here
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedFile: null,
+      result: {}
+    };
+  }
+  onChangeHandler = event => {
+    this.setState({
+      selectedFile: event.target.files[0],
+      loaded: 0
+    });
+  };
+
+  onClickHandler = () => {
+    const data = new FormData();
+    data.append('file', this.state.selectedFile);
+    axios
+      .post(
+        'https://us-central1-mmx-programmer.cloudfunctions.net/upload',
+        data
+      )
+      .then(res => {
+        // then print response status
+        console.log(res.statusText);
+        this.setState({ result: res.data });
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  };
+
+  render() {
+    return (
+      <div>
+        <form>
+          <input type="file" name="file" onChange={this.onChangeHandler} />
+          <button type="button" onClick={this.onClickHandler}>
+            Upload
+          </button>
+        </form>
+        <a href={process.env.PUBLIC_URL + '/example.mid'}>Example midi file</a>
+        <pre>{JSON.stringify(this.state.result, null, 2)}</pre>
+      </div>
+    );
+  }
 }
-
-export default App;
